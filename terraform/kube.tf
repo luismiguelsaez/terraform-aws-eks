0fa2d60c607cf33ed1e9caaedf34a31ec9db0416
luismiguelsaez <luismiguelsaez@protonmail.com>
2020-06-05 17:00:41 +0200

    Added providers version

--- terraform/kube.tf
+++ terraform/kube.tf
@@ -1,4 +1,5 @@
 provider "kubernetes" {
+  version = "~> 1.11.2"
   host                   = aws_eks_cluster.main.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.main.token
@@ -6,6 +7,7 @@ provider "kubernetes" {
 }
 
 provider "helm" {
+  version = "~> 1.2.2"
   kubernetes {
     host                   = aws_eks_cluster.main.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority.0.data)
