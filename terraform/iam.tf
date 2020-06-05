14cb66e47862e8b5d62a2dd39da5ec7dc2dbd160
luismiguelsaez <luismiguelsaez@protonmail.com>
2020-06-05 11:59:22 +0200

    Added OIDC and role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,4 @@
+### Create EKS cluster role
 resource "aws_iam_role" "eks" {
   name = format("eks-cluster-%s", var.defaults.environment)
 
@@ -23,6 +24,7 @@ resource "aws_iam_role_policy_attachment" "eks-service" {
   role       = aws_iam_role.eks.name
 }
 
+### Create EKS nodes role
 resource "aws_iam_role" "node" {
   name = format("eks-nodes-%s", var.defaults.environment)
 
@@ -51,4 +53,34 @@ resource "aws_iam_role_policy_attachment" "eks-node-cni" {
 resource "aws_iam_role_policy_attachment" "eks-node-registry" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.node.name
+}
+
+### Enable OIDC provider
+resource "aws_iam_openid_connect_provider" "main" {
+  client_id_list  = [ "sts.amazonaws.com"] 
+  thumbprint_list = []
+  url             = aws_eks_cluster.main.identity.0.oidc.0.issuer
+}
+
+data "aws_iam_policy_document" "oidc" {
+  statement {
+    actions = [ "sts:AssumeRoleWithWebIdentity" ]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.main.url, "https://", "")}:sub"
+      values   = [ "system:serviceaccount:kube-system:aws-node" ]
+    }
+
+    principals {
+      identifiers = [ aws_iam_openid_connect_provider.main.arn ]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "oidc" {
+  name               = format("eks-cluster-oidc-%s", var.defaults.environment)
+  assume_role_policy = data.aws_iam_policy_document.oidc.json
 }
