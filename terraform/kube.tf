0f072111bbf6888c19b9a830d6150bda10e90d38
luismiguelsaez <luismiguelsaez@protonmail.com>
2020-06-05 16:56:38 +0200

    Added helm provider

--- terraform/kube.tf
+++ terraform/kube.tf
@@ -5,6 +5,17 @@ provider "kubernetes" {
   load_config_file       = false
 }
 
+provider "helm" {
+  tiller_image = "gcr.io/kubernetes-helm/tiller:v2.9.1"
+
+  kubernetes {
+    host                   = aws_eks_cluster.main.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.main.token
+    load_config_file       = false
+  }
+}
+
 resource "kubernetes_service_account" "oidc" {
   metadata {
     name = "aws-node"
