2ff761a2c51fcbc66b0730d9386e86b3b45f709b
luismiguelsaez <luismiguelsaez@protonmail.com>
2020-06-06 09:27:56 +0200

    Modified resources names

--- terraform/iam-eks-role-s3.tf
+++ terraform/iam-eks-role-s3.tf
@@ -1,18 +1,18 @@
-resource "aws_iam_policy" "s3_test" {
-  name        = format("%s-s3_test", var.defaults.environment)
+resource "aws_iam_policy" "s3" {
+  name        = format("%s-s3", var.defaults.environment)
   description = "S3 access test policy"
   policy      = file("aws/iam/policy/s3_test.json")
 }
 
-resource "aws_iam_role" "s3_test" {
-   name = format("%s-s3_test", var.defaults.environment)
+resource "aws_iam_role" "s3" {
+   name = format("%s-s3", var.defaults.environment)
    assume_role_policy = jsonencode({
     Statement = [{
       Action = "sts:AssumeRoleWithWebIdentity"
       Effect = "Allow"
       Condition = {
         StringEquals = {
-          "${replace(aws_iam_openid_connect_provider.main.url, "https://", "")}:sub" = "system:serviceaccount:default:s3-test"
+          "${replace(aws_iam_openid_connect_provider.main.url, "https://", "")}:sub" = "system:serviceaccount:default:s3"
         }
       }
       Principal = {
@@ -23,17 +23,17 @@ resource "aws_iam_role" "s3_test" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "s3_test" {
-  role       = aws_iam_role.s3_test.name 
-  policy_arn = aws_iam_policy.s3_test.arn
+resource "aws_iam_role_policy_attachment" "s3" {
+  role       = aws_iam_role.s3.name 
+  policy_arn = aws_iam_policy.s3.arn
 }
 
 resource "kubernetes_service_account" "oidc" {
   metadata {
-    name = "s3-test"
+    name = "s3"
     namespace = "default"
     annotations = {
-      "eks.amazonaws.com/role-arn" = aws_iam_role.s3_test.arn
+      "eks.amazonaws.com/role-arn" = aws_iam_role.s3.arn
     }
   }
 }
